04e7f06fc5a6f865577002d557653914eec6384b
wuruijie <callcicle@sina.com>
2016-03-02 10:11:07 +0800

    wo 提交

---
+++ TestCer/TestCer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestCer
+//
+//  Created by 刘清 on 16/3/1.
+//  Copyright © 2016年 LQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
